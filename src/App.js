0418591655236c139166ae254e5b7b3ea9e50ddf
OrisonXmef <110114092+OrisonXmef@users.noreply.github.com>
2022-08-11 17:18:53 +0000

    Add files via upload
    My reactapp

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import NavBar from './components/NavBar'
+function App() {
+  return (
+    <div>
+      <form/>
+      <NavBar/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/NavBar.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function NavBar() {
+  return (
+    <div>
+        <h1> Welcome to Sadika Shop</h1>
+        <nav>
+            <div>
+                <button>Submit</button>
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li> Products</li>
+                    <li>Contact</li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+  )
+}
+
+export default NavBar
